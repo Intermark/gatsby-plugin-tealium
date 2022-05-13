58b7047ab5bfe0bc586818121a4a467dd6d3aa58
Stu Finn <stu@borealisweb.ca>
2022-05-13 16:31:49 -0500

    Update utagData schema + add Joi descriptions

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,13 +1,22 @@
-// TODO: Add descriptions
+// TODO: Add remaining descriptions
 export const pluginOptionsSchema = ({ Joi }) => {
   return Joi.object({
-    account: Joi.string().required().description(``),
-    profile: Joi.string().required().description(``),
-    utagData:Joi.string().required().description(``),
-    env: Joi.string().valid("dev", "qa", "prod").description(``).default(`default message`),
+    account: Joi.string()
+      .required()
+      .description(`Tealium account name, e.g. "companyXYZ"`),
+    profile: Joi.string()
+      .required()
+      .description(`Tealium profile name, e.g. "main"`),
+    utagData: Joi.object().description(
+      `Initial value for the Universal Data Object (UDO). If not provided, a UDO will not be set automatically. An empty object is accepted as a value.`
+    ),
+    env: Joi.string()
+      .valid("dev", "qa", "prod")
+      .description(`Tealium environment name, e.g. "prod"`)
+      .default(`default message`),
     injectUtagSync: Joi.boolean().description(``).default(true),
-    disableInitialTracking: Joi.boolean().description(``).default(`default message`)
-  }).external(async (pluginOptions) => {
+    disableInitialTracking: Joi.boolean().description(``).default(false),
+  }).external(async pluginOptions => {
     const url = `https://tags.tiqcdn.com/utag/${pluginOptions.account}/${pluginOptions.profile}/${pluginOptions.env}/utag.sync.js`
     try {
       const res = await fetch(url)
@@ -16,7 +25,7 @@ export const pluginOptionsSchema = ({ Joi }) => {
     } catch (err) {
       throw new Error(
         `Cannot access Tealium utag at "${url}". Please check your gatsby-plugin-tealium settings and try again!`
-      );
+      )
     }
-  });
-};
+  })
+}
