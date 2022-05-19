43d3d4e89f20298e550dab932fb0c05300075742
Stu Finn <stu@borealisweb.ca>
2022-05-19 14:45:10 -0500

    Fix tealium link event not firing

--- hooks/useTealiumViewEvent.ts
+++ hooks/useTealiumViewEvent.ts
@@ -1,12 +1,10 @@
 import { TealiumEvent } from "../global.d"
 
 const useTealiumViewEvent = (data: TealiumEvent): void => {
-  console.log("Tealium view event", data)
   if (!window || !window.utag || !data) {
     console.error("utag.js has not loaded yet.")
     return
   }
-  console.log("UTAG has loaded!", window.utag)
   window.utag.view(data)
 }
 export default useTealiumViewEvent
